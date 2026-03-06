8d180f33e28ef10be42ca981d49c6a4b172ca148
lndshrk83 <lndshrk.83@gmail.com>
2026-03-06 12:55:19 -0500

    Update suppliers.js

--- backend/src/routes/suppliers.js
+++ backend/src/routes/suppliers.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import { 
   getAllSuppliers, 
-  createSupplier 
+  createSupplier,
+  updateSupplier,
+  deleteSupplier
 } from '../controllers/suppliersController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllSuppliers);
-router.post('/', createSupplier);
+router.post('/', protect, createSupplier);
+router.put('/:id', protect, updateSupplier);
+router.delete('/:id', protect, deleteSupplier);
 
 export default router;
