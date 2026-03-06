68655c8a1fc4dd03d2508c30a247a8490ef1d17e
lndshrk83 <lndshrk.83@gmail.com>
2026-03-06 12:38:55 -0500

    Update suppliers.js

--- backend/src/routes/suppliers.js
+++ backend/src/routes/suppliers.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { getAllSuppliers } from '../controllers/suppliersController.js';
+import { 
+  getAllSuppliers, 
+  createSupplier 
+} from '../controllers/suppliersController.js';
 
 const router = express.Router();
 
 router.get('/', getAllSuppliers);
+router.post('/', createSupplier);
 
 export default router;
