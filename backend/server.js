734dd958f090daa714a87aef6b13ab2a96c9ceb0
lndshrk83 <lndshrk.83@gmail.com>
2026-03-06 12:27:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-import productsRouter from './src/routes/products.js';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { pool } from './src/config/db.js';
+import productsRouter from './src/routes/products.js';
+import suppliersRouter from './src/routes/suppliers.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/products', productsRouter);
 
 app.get('/', (req, res) => {
   res.send('Inventory Management System API is running');
 });
+
+app.use('/products', productsRouter);
+app.use('/suppliers', suppliersRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
