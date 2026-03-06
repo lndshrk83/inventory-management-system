1058411aa1e8b978b6a4477c52abf322aeda9bb5
lndshrk83 <lndshrk.83@gmail.com>
2026-03-06 12:31:27 -0500

    Create suppliers.js

---
+++ backend/src/routes/suppliers.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getAllSuppliers } from '../controllers/suppliersController.js';
+
+const router = express.Router();
+
+router.get('/', getAllSuppliers);
+
+export default router;
